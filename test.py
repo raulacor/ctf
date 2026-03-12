577bd522e188847d13774453f9ad53eb25c1eb34
raulacor <raul.acorsi@gmail.com>
2026-03-11 22:12:29 -0300

    Maybe it would be better if this was a bash script

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import os
 #https://pynput.readthedocs.io/en/latest/
 
-os.system("pwd=$(pwd)") #needed for eyewitness
 
 print("#####################################################################################")
 print("#                                                                                   #")
@@ -22,12 +21,12 @@ def setup():
     os.system("mkdir thirdlevels scans eyewitness")
     #os.system("apt install subfinder")
 
-def initial_recon(target):
+def initial_recon(target, full_url):
     print("Running whois. . .")
     os.system(f"whois {target} >> whois.txt")
 
     print("Searching web directories with ffuf. . .")
-    os.system(f"ffuf -u {target}/FUZZ -w /usr/share/wordlists/dirb/common.txt")
+    os.system(f"ffuf -u {full_url}/FUZZ -w /usr/share/wordlists/dirb/common.txt >> ffuf.txt")
 
     print("Running curl. . .")
     os.system(f"curl -I {target} -o curl.txt")
@@ -38,7 +37,7 @@ def subfinders(target, pwd):
     os.system(f"echo {target} >> final.txt")
 
     print("Compiling third-level domains. . .")
-    os.system("cat final.txt | grep -Po '(\w+\.\w+\.\w+)$' | sort -u >> third-level.txt")
+    os.system("cat final.txt | grep -Po ""(\w+\.\w+\.\w+)$"" | sort -u >> third-level.txt")
     ##https://regex101.com
 
     print("Gathering full third-level domains. . .")
@@ -46,21 +45,22 @@ def subfinders(target, pwd):
     os.system("cat thirdlevels/domain.txt | sort -u >> final.txt")
 
     print("Searching for live subdomains. . .")
-    os.system("cat final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' > probed.txt") 
+    os.system("cat final.txt | sort -u | httprobe -s -p https:443 | sed ""s/https\?:\/\///"" | tr -d ':443' > probed.txt") 
     #by limiting the port we get to only find the subdomains that actually resolve by the router.
 
     print("Scanning for open ports. . .")
     os.system("nmap -p- -iL probed.txt -T4 -sV -oA scans/scanned.txt")
 
     print("Running eyewitness. . .")
-    os.system(f"eyewitness -f {pwd} probed.txt -d {target}")
+    os.system(f"eyewitness -f {pwd}probed.txt -d {target}")
 
 def main():
     target = input("\033[32m{}\033[0m".format("Input target: "))
+    full_url = f"https://{target}"
     pwd = input("\033[32m{}\033[0m".format("Input folder path: [e.t. ~/usr/bin/]"))
     setup()
-    initial_recon()
-    subfinders()
+    initial_recon(target, full_url)
+    subfinders(target, pwd)
 
 if __name__ == "__main__":
     main()
